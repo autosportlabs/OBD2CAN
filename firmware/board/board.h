f696f1dacc341d166034375115e8f304bb252f5b
Brent Picasso <brent@autosportlabs.com>
2016-09-01 12:22:04 -0700

    enable HSE.. maybe?

--- firmware/board/board.h
+++ firmware/board/board.h
@@ -35,7 +35,7 @@
 /*
  * Board oscillators-related settings.
  * NOTE: LSE not fitted.
- * NOTE: HSE not fitted.
+ * NOTE: HSE is fitted.
  */
 #if !defined(STM32_LSECLK)
 #define STM32_LSECLK                0U
@@ -44,10 +44,10 @@
 #define STM32_LSEDRV                (3U << 3U)
 
 #if !defined(STM32_HSECLK)
-#define STM32_HSECLK                0U
+#define STM32_HSECLK                8000000U
 #endif
 
-#define STM32_HSE_BYPASS
+/* #define STM32_HSE_BYPASS */
 
 /*
  * MCU type as defined in the ST header.
