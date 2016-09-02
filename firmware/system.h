b074f3b0a72049ec0391429c1378ed8b5b9a67a8
Brent Picasso <brent@autosportlabs.com>
2016-09-01 22:58:08 -0700

    added license headers

--- firmware/system.h
+++ firmware/system.h
@@ -1,9 +1,24 @@
 /*
- * system.h
+ * OBD2CAN firmware
  *
- *  Created on: Aug 31, 2016
- *      Author: brent
+ * Copyright (C) 2016 Autosport Labs
+ *
+ * This file is part of the Race Capture firmware suite
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should
+ * have received a copy of the GNU General Public License along with
+ * this code. If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #ifndef SYSTEM_H_
 #define SYSTEM_H_
